2df97d9343c2c7a34d71b588d1f2463f85f3355d
Gokul Falke <145555165+gokulfalke17@users.noreply.github.com>
2025-12-19 13:45:59 +0530

    EMP-102 :: Add Employee Controller with basic operations

---
+++ src/main/java/com/company/employee/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.company.employee;
+
+import java.util.List;
+
+public class EmployeeController {
+
+    private final EmployeeService employeeService = new EmployeeService();
+
+    public void createEmployee(Employee employee) {
+        employeeService.addEmployee(employee);
+        System.out.println("Employee added successfully");
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
